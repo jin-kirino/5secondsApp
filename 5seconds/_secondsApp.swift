597b19d3def96371dab8ea75441ddb350f6c1045
jin-kirino <jinkirino@gmail.com>
2023-06-05 22:42:24 +0900

    first push

---
+++ 5seconds/_secondsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _secondsApp.swift
+//  5seconds
+//
+//  Created by 神　樹里乃 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct _secondsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
