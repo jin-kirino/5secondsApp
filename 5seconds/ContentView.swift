8c5cd233cb35060ddc01d3bd0fbe857934d72f81
jin-kirino <jinkirino@gmail.com>
2023-06-07 07:41:03 +0900

    ボタンを4つ配置

--- 5seconds/ContentView.swift
+++ 5seconds/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            SelectButtonsLayout()
         }
-        .padding()
+    }
+}
+
+struct SelectButtonsLayout: View {
+    var body: some View {
+        VStack {
+            HStack {
+                ButtonRowLayout(buttonName: "Level 1")
+                ButtonRowLayout(buttonName: "Level 2")
+            }
+            HStack {
+                ButtonRowLayout(buttonName: "Level 3")
+                ButtonRowLayout(buttonName: "Level 4")
+            }
+        }
+    }
+}
+
+struct ButtonRowLayout: View {
+    var buttonName: String
+
+    var body: some View {
+        Button {
+            // ゲーム画面を表示する
+
+        } label: {
+            Text(buttonName)
+        }
     }
 }
 
